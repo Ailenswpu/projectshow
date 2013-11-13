8b608bb07332ef0b3e5e58914ea0e8f0cc18b241
Ailenswpu <renkaiswpu@gmail.com>
2013-11-13 21:02:32 +0800

    update

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,13 +1,14 @@
 # encoding : utf-8
 class Project < ActiveRecord::Base
   validates :description, :length => { :maximum => 120 }
-  
+    
   belongs_to :user
   mount_uploader :video, FileUploader
     
-  rails_admin do
-  	field :name
-  	field :description, :text
+  rails_admin do        
+    field :id
+    field :name
+    field :description, :text
     field :declaration, :ck_editor
     field :assignment_book, :ck_editor
     field :phase_check, :ck_editor
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,16 @@ class User < ActiveRecord::Base
   
   has_one :project
   
+  class << self
+     def current_user=(user)
+       Thread.current[:current_user] = user
+     end
+
+     def current_user
+       Thread.current[:current_user]
+     end
+   end
+   
   rails_admin do
     #visible false
     label_plural '管理员管理'
